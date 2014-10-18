704b0792b5be591056e9f87805f7d319dd9f455a
bxav <xavier.b@zoho.com>
2014-10-18 11:37:01 +0200

    Add some new demo methods

--- src/Bxav/Bundle/ExtraSoapApiBundle/Service/DemoService.php
+++ src/Bxav/Bundle/ExtraSoapApiBundle/Service/DemoService.php
@@ -6,6 +6,78 @@ class DemoService
     public $test = "coucou"; 
 
     /**
+     * 
+     * @param string $from
+     * @param string $to
+     * @param string $object
+     * @param string $body
+     * @return boolean
+     */
+    public function sendMail($from, $to, $object, $body)
+    {
+        $valid = true;
+        return $valid;
+    }
+    
+    /**
+     * 
+     * @param string $instanceName
+     * @return boolean
+     */
+    public function installCRM($instanceName)
+    {
+        $valid = true;
+        return $valid;
+    }
+    
+    /**
+     * 
+     * @param string $instanceName
+     * @return boolean
+     */
+    public function buildCRM($instanceName)
+    {
+        $valid = true;
+        sleep(5);
+        return $valid;
+    }
+    
+    /**
+     *
+     * @param string $instanceName
+     * @return boolean
+     */
+    public function buildWebsite($instanceName)
+    {
+        $valid = true;
+        sleep(10);
+        return $valid;
+    }
+    
+    /**
+     *
+     * @param string $instanceName
+     * @return boolean
+     */
+    public function buildDataBox($instanceName)
+    {
+        $valid = true;
+        return $valid;
+    }
+    
+    /**
+     *
+     * @param int $spaceInGB
+     * @param string $instanceName
+     * @return boolean
+     */
+    public function addSpaceToDataBox($spaceInGB, $instanceName)
+    {
+        $valid = true;
+        return $valid;
+    }
+    
+    /**
      *
      * @return array $services
      */
